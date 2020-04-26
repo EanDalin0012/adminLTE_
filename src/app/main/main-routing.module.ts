47856ff0598ac47396304fd789d6c1fde072e1fa
EanDalin0012 <endalined@gmail.com>
2020-04-26 14:08:27 +0700

    add import-product to main-module

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -5,13 +5,20 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('../home/home.module').then( m => m.HomeModule)
   },
-  { path: 'register',
-    children: [ { path: '', loadChildren: () => import('../home/home.module').then( m => m.HomeModule) }]
+  { path: '',
+    children: [ { path: 'register', loadChildren: () => import('../home/home.module').then( m => m.HomeModule) }]
   },
   { path: '',
     children: [
       { path: 'dash-board',
         loadChildren: () => import('../dash-board/dash-board.module').then( m => m.DashBoardModule)
+      }
+    ]
+  },
+  { path: '',
+    children: [
+      { path: 'import-product',
+        loadChildren: () => import('../import-product/import-product.module').then( m => m.ImportProductModule)
       }
     ]
   }
